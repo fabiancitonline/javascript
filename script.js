d77ed4e003df4e99328a2acd62bd714784f81c23
Fabian Bariandaran <fabianbdr@icloud.com>
2022-01-03 20:07:27 -0300

    desafio objetos

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+class comprador{
+    constructor(nombre, apellido, telefono, ciudad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.ciudad = ciudad;
+    }
+    mostrar(){
+var he = document.createElement("H1")
+var te = document.createTextNode("Su nombre es: " + this.nombre + " " + this.apellido + ", su telefono es: " + this.telefono + " y su ciudad es: " + this.ciudad)
+he.appendChild(te)
+document.body.appendChild(he);
+    }
+}
+
+let name = prompt("Ingrese su nombre");
+let lastname = prompt("Ingrese su apellido");
+let tel = prompt("Ingrese su teléfono");
+let city = prompt("Ingrese su ciudad");
+const comprador1 = new comprador(name, lastname, tel, city);
+comprador1.mostrar();
+
 let valor = prompt("Ingrese el valor del producto que desea comprar");
 let cant = prompt("Ingrese la cantidad de productos iguales que lleva");
 let total = valor * cant;
